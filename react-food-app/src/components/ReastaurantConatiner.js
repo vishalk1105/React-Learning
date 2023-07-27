c8d8a71359f0b0eec37f5df73c758a1dcf26ac87
Vishalk1105 <vishalvkamble95@gmail.com>
2023-07-27 11:27:07 +0530

    Saerch Feature

--- react-food-app/src/components/ReastaurantConatiner.js
+++ react-food-app/src/components/ReastaurantConatiner.js
@@ -4,39 +4,74 @@ import { useEffect, useState } from "react";
 import ShimmerUI from "./ShimmerUI";
 const ReastaurantConatiner = () => {
   const [resListData, setResListData] = useState([]);
-
+  const [filteredData, setFilteredData] = useState([]);
+  const [searchText, setSearchText] = useState("");
   useEffect(() => {
     fetchData();
-  });
+  }, []);
 
   const fetchData = async () => {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
     );
     const jsonData = await data.json();
-    setResListData(jsonData?.data?.cards[2]?.data?.data?.cards);
+    // console.log(
+    //   jsonData.data.cards[2].card.card.gridElements.infoWithStyle.restaurants
+    // );
+    setResListData(
+      jsonData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+    setFilteredData(
+      jsonData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
   };
 
-  if (resListData.length === 0) {
+  if (resListData?.length === 0) {
     return <ShimmerUI />;
   }
+
   return (
     <div className="body">
-      <div>
-        <button
-          onClick={() => {
-            let filteredList = resListData.filter(
-              (res) => res.data.avgRating >= 4
-            );
-            setResListData(filteredList);
-          }}
-        >
-          Filter By Rating 1
-        </button>
+      <div className="filter">
+        <div>
+          <input
+            type="text"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              const filteredres = resListData.filter((res) =>
+                res.info.name
+                  .toLowerCase()
+                  .includes(searchText.toLocaleLowerCase())
+              );
+              setFilteredData(filteredres);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              let filteredList = resListData.filter(
+                (res) => res.info.avgRating >= 4
+              );
+              setResListData(filteredList);
+            }}
+          >
+            Filter By Rating 1
+          </button>
+        </div>
       </div>
       <div className="res_container">
-        {resListData.map((data) => (
-          <RestaurantCard key={data.data.id} resData={data.data} />
+        {filteredData.map((data) => (
+          <RestaurantCard key={data.info.id} resData={data.info} />
         ))}
       </div>
     </div>
